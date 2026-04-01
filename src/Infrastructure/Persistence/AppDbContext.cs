3f898b806cde981e57e4525675b355becf06a338
Guilherme Cavalcanti <gui.arcoverde21@gmail.com>
2026-04-01 17:31:04 -0300

    feat: implement IAppDbContext and add Transactions DbSet

--- src/Infrastructure/Persistence/AppDbContext.cs
+++ src/Infrastructure/Persistence/AppDbContext.cs
@@ -1,11 +1,15 @@
 namespace Aerarium.Infrastructure.Persistence;
 
+using Aerarium.Application.Common;
+using Aerarium.Domain.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options)
-    : IdentityDbContext(options)
+    : IdentityDbContext(options), IAppDbContext
 {
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
